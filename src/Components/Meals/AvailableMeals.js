b9f4a40367f11e2df2aa1f36cf9c0bdedfcf0a93
Jay Nimeshkumar Patel <jy982893@dal.ca>
2022-01-21 18:44:47 -0400

    added error handling and loading message

--- src/Components/Meals/AvailableMeals.js
+++ src/Components/Meals/AvailableMeals.js
@@ -5,38 +5,55 @@ import MealItem from "./MealItem/MealItem";
 
 const AvailableMeals = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState();
 
   useEffect(() => {
     fetchMeals();
   }, []);
 
   async function fetchMeals() {
-    const response = await fetch(
-      "https://react-wasd-default-rtdb.firebaseio.com/meals.json/"
-    );
-    const data = await response.json();
-    setMeals(
-      data.length
-        ? data.map((meal) => {
-            return (
-              <MealItem
-                id={meal.id}
-                name={meal.name}
-                key={meal.id}
-                description={meal.description}
-                price={meal.price}
-              />
-            );
-          })
-        : []
-    );
+    try {
+      const response = await fetch(
+        "https://react-wasd-default-rtdb.firebaseio.com/meals.json/"
+      );
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const data = await response.json();
+      setMeals(
+        data.length
+          ? data.map((meal) => {
+              return (
+                <MealItem
+                  id={meal.id}
+                  name={meal.name}
+                  key={meal.id}
+                  description={meal.description}
+                  price={meal.price}
+                />
+              );
+            })
+          : []
+      );
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }
+
+  let content;
+  if (isLoading) {
+    content = <p>Fetching Meals..</p>;
+  } else if (error) {
+    content = <p>{error}</p>;
+  } else {
+    content = <ul>{meals}</ul>;
   }
 
   return (
     <section className={classes.meals}>
-      <Card>
-        <ul>{meals}</ul>
-      </Card>
+      <Card>{content}</Card>
     </section>
   );
 };
